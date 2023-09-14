f1ff436a855b370f788c365b436f11f35c2586d2
Abi Arasy Drajat Prakoso <abiarasy096@gmail.com>
2023-09-14 17:30:41 +0700

    mobileprogrammingweek3

---
+++ bin/project3abi.dart
@@ -0,0 +1,5 @@
+import 'package:project3abi/project3abi.dart' as project3abi;
+
+void main(List<String> arguments) {
+  print('Hello world: ${project3abi.calculate()}!');
+}
---
+++ test/project3abi_test.dart
@@ -0,0 +1,8 @@
+import 'package:project3abi/project3abi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
